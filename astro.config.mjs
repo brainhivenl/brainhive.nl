d7f756846edbf1b7a7df62c73394df58a8b5f69b
Dillen Meijboom <work@dillen.dev>
2023-05-16 09:28:38 +0200

    feat: add a sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import image from "@astrojs/image";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://brainhive.nl",
   integrations: [tailwind(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  })]
+  }), sitemap()]
 });
